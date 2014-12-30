8261fd1524589d42b46fe1f6d7af4155b3cca7ec
Maxime Thirouin <m@moox.io>
2014-12-30 09:58:25 +0100

    use loadCSS & loadJS  lib

--- index.js
+++ index.js
@@ -250,10 +250,7 @@
    */
   function loadCustomScripts(scriptUrls) {
     scriptUrls.forEach(function(url) {
-      var scriptEl = document.createElement("script")
-      scriptEl.setAttribute("src", url)
-
-      document.body.appendChild(scriptEl)
+      loadJS(url)
     })
   }
 
@@ -262,12 +259,48 @@
    */
   function loadCustomStyles(styleUrls) {
     styleUrls.forEach(function(url) {
-      var linkEl = document.createElement("link")
-      linkEl.setAttribute("rel", "stylesheet")
-      linkEl.setAttribute("href", url)
-
-      document.body.appendChild(linkEl)
+      loadCSS(url)
     })
   }
 
+  // https://github.com/filamentgroup/loadJS/blob/master/loadJS.js
+  /*! loadJS: load a JS file asynchronously. [c]2014 @scottjehl, Filament Group, Inc. (Based on http://goo.gl/REQGQ by Paul Irish). Licensed MIT */
+  function loadJS(src, cb) {
+    "use strict";
+    var ref = window.document.getElementsByTagName("script")[ 0 ]
+    var script = window.document.createElement("script")
+    script.src = src
+    script.async = true
+    ref.parentNode.insertBefore(script, ref)
+    if (cb) {
+      script.onload = cb
+    }
+    return script
+  }
+
+  // https://github.com/filamentgroup/loadCSS/blob/master/loadCSS.js
+  /*! loadCSS: load a CSS file asynchronously. [c]2014 @scottjehl, Filament Group, Inc. Licensed MIT */
+  function loadCSS(href, before, media) {
+    "use strict";
+    // Arguments explained:
+    // `href` is the URL for your CSS file.
+    // `before` optionally defines the element we'll use as a reference for injecting our <link>
+    // By default, `before` uses the first <script> element in the page.
+    // However, since the order in which stylesheets are referenced matters, you might need a more specific location in your document.
+    // If so, pass a different reference element to the `before` argument and it'll insert before that instead
+    // note: `insertBefore` is used instead of `appendChild`, for safety re: http://www.paulirish.com/2011/surefire-dom-element-insertion/
+    var ss = window.document.createElement("link")
+    var ref = before || window.document.getElementsByTagName("script")[0]
+    ss.rel = "stylesheet"
+    ss.href = href
+    // temporarily, set media to something non-matching to ensure it'll fetch without blocking render
+    ss.media = "only x"
+    // inject link
+    ref.parentNode.insertBefore(ss, ref)
+    // set media back to `all` so that the stylesheet applies once it loads
+    setTimeout(function() {
+      ss.media = media || "all"
+    })
+    return ss
+  }
 })(window.putaindeTab)
